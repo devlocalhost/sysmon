dcc12070ef474b9ae3f26f18e3ce0f1214c8057c
devlocalhost <lazarecobani@gmail.com>
2023-08-23 00:00:05 +0300

    plugins/extra.py: remove duplicate comments

--- plugins/extra.py
+++ plugins/extra.py
@@ -11,24 +11,19 @@ import glob
 
 # START OF IMPORTANT PART FOR THE PLUGINS
 
-# general vars
 # general vars, save data directory and IEC or metric conversion (size)
 SAVE_DIR = None
 CONVERSION_TYPE = 1024
 
-# cpuinfo.py vars
 # cpuinfo.py vars - control whenever to show cpu temperature
 SHOW_TEMPERATURE = True
 
-# meminfo.py vars
 # meminfo.py vars - show swap statsor no
 SHOW_SWAP = True
 
-# procpid.py vars
 # procpid.py vars - how many processes to display
 PROCS = 6
 
-# netstats vars
 # netstats vars - custom interface, from /sys/class/net/
 # check /sys/class/net/ for more
 INTERFACE = None
