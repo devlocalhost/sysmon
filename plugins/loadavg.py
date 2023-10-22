e71d4b2d1d947448629de9d780fd1f7c7d8ce2c6
devlocalhost <lazarecobani@gmail.com>
2023-10-22 16:41:54 +0300

    plugins/*: new file reading method [1/??]
    wip, not fully done yet, thanks to NecrosS

--- plugins/loadavg.py
+++ plugins/loadavg.py
@@ -2,12 +2,33 @@
 
 """loadavg plugin for sysmon"""
 
+import logging
 import time
 import sys
 
 from datetime import datetime
 from util.util import en_open
 
+logging.basicConfig(
+    filename="logs/loadavg.log",
+    level=logging.DEBUG,
+    format="%(asctime)s: %(message)s",
+)
+logger = logging.getLogger()
+
+try:
+    file = en_open("/proc/loadavg")
+    logger.info("file opened")
+
+except FileNotFoundError:
+    logger.warning("file not found")
+    sys.exit("Couldn't find /proc/loadavg file")
+
+except PermissionError:
+    logger.error("file cant read")
+    sys.exit(
+        "Couldn't read the file. Do you have read permissions for /proc/loadavg file?"
+    )
 
 def uptime_format():
     """format the uptime from seconds to a human readable format"""
@@ -39,29 +60,24 @@ def uptime_format():
 def main():
     """/proc/loadavg - system load times and uptime"""
 
-    try:
-        with en_open("/proc/loadavg") as loadavg_file:
-            loadavg_data = loadavg_file.read().split()
-            onemin, fivemin, fiveteenmin = loadavg_data[:3]
-            entities_active, entities_total = loadavg_data[3].split("/")
+    logger.info(" file read")
 
-            uptime_func_out = uptime_format()
+    loadavg_data = file.read().split()
+    onemin, fivemin, fiveteenmin = loadavg_data[:3]
+    entities_active, entities_total = loadavg_data[3].split("/")
 
-            up_since_fmt = datetime.fromtimestamp(
-                time.time() - uptime_func_out[1]
-            ).strftime("%A %B %d %Y, %I:%M:%S %p")
+    uptime_func_out = uptime_format()
 
-        return (
-            f"  ——— /proc/loadavg {'—' * 47}\n"
-            f"     Load: {onemin}, {fivemin}, {fiveteenmin}"
-            f"{' ':<6}| Procs: {entities_active} executing, {entities_total} total"
-            f"\n   Uptime: {uptime_func_out[0]}\n   Booted: {up_since_fmt}\n"
-        )
+    up_since_fmt = datetime.fromtimestamp(
+        time.time() - uptime_func_out[1]
+    ).strftime("%A %B %d %Y, %I:%M:%S %p")
 
-    except FileNotFoundError:
-        sys.exit("Couldn't find /proc/loadavg file")
+    file.seek(0)
+    logger.info(" data out")
 
-    except PermissionError:
-        sys.exit(
-            "Couldn't read the file. Do you have read permissions for /proc/loadavg file?"
-        )
+    return (
+        f"  ——— /proc/loadavg {'—' * 47}\n"
+        f"     Load: {onemin}, {fivemin}, {fiveteenmin}"
+        f"{' ':<6}| Procs: {entities_active} executing, {entities_total} total"
+        f"\n   Uptime: {uptime_func_out[0]}\n   Booted: {up_since_fmt}\n"
+    )
