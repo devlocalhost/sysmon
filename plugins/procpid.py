8126545929d32c86c05fc0d1b86eb5423f366cc1
devlocalhost <lazarecobani@gmail.com>
2023-10-27 22:46:01 +0300

    plugins/procpid.py: show sysmon stats

--- plugins/procpid.py
+++ plugins/procpid.py
@@ -12,60 +12,73 @@ from util.util import (
 from util.logger import setup_logger
 
 logger = setup_logger("procpid")
+sysmon_pid = os.getpid()
+
+
+def read_process_status(pid):
+    """get pid data, like name, state, vmrss"""
+
+    try:
+        with en_open(f"/proc/{pid}/status") as status_file:
+            process_info = {}
+            vmrss_found = False
+            process_info["pid"] = pid
+
+            for line in status_file:
+                parts = line.split()
+                if len(parts) >= 2:
+                    key = parts[0].rstrip(":")
+                    if (
+                        key == "Name"
+                        or key == "State"
+                        or (key == "VmRSS" and len(parts) >= 3)
+                    ):
+                        if key != "State":
+                            value = parts[1]
+                        else:
+                            state_part = (
+                                parts[2].partition("(")[2].partition(")")[0].title()
+                            )
+                            value = state_part
+                        process_info[key] = value
+
+                        if key == "VmRSS":
+                            vmrss_found = True
+
+            if vmrss_found:
+                return process_info
+    except FileNotFoundError:
+        pass
+
+    return None
 
 
 def main():
-    """return the most ram (vmrss) consuming pids, with their state and name"""
+    """Return the most RAM (VmRSS) consuming PIDs, with their state and name"""
 
     process_dirs = [pid for pid in os.listdir("/proc") if pid.isdigit()]
     processes = []
 
     for pid in process_dirs:
-        try:
-            with en_open(f"/proc/{pid}/status") as status_file:
-                process_info = {}
+        process_info = read_process_status(pid)
+        if process_info:
+            processes.append(process_info)
 
-                vmrss_found = False
-                process_info["pid"] = pid
-
-                for line in status_file:
-                    parts = line.split()
-                    if len(parts) >= 2:
-                        key = parts[0].rstrip(":")
-                        if (
-                            key == "Name"
-                            or key == "State"
-                            or (key == "VmRSS" and len(parts) >= 3)
-                        ):
-                            if key != "State":
-                                value = parts[1]
-                            else:
-                                state_part = (
-                                    parts[2].partition("(")[2].partition(")")[0].title()
-                                )
-                                value = state_part
-                            process_info[key] = value
-
-                            if key == "VmRSS":
-                                vmrss_found = True
-
-                if vmrss_found:
-                    processes.append(process_info)
-
-        except FileNotFoundError:
-            pass
-
-    processes = sorted(
-        processes, key=lambda x: int(x.get("VmRSS", 0)), reverse=True
-    )
+    processes = sorted(processes, key=lambda x: int(x.get("VmRSS", 0)), reverse=True)
 
     formatted_data = [
-            f"  ——— /proc/pid/status (self: {os.getpid():<5}) {'—' * 30}\n   Name            PID         RSS            State"
+        f"  ——— /proc/pid/status (self: {os.getpid():<5}) {'—' * 30}\n   Name            PID         RSS            State"
     ]
 
-    for procs_data in processes[:PROCS]:
+    if sysmon_pid is not None:
+        sysmon_info = read_process_status(sysmon_pid)
+        if sysmon_info:
+            sysmon_info["Name"] = "sysmon"
+            processes.insert(0, sysmon_info)
+
+    for procs_data in processes[: PROCS + 1]:
         process_name = procs_data["Name"]
-        pid = procs_data["pid"]
+        pid = str(procs_data["pid"])
         rss_usage = convert_bytes(to_bytes(int(procs_data["VmRSS"])))
         pstate = procs_data["State"]
 
