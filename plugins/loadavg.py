a0488e29e8bb4857793d58e61c1b23d90de680ee
devlocalhost <lazarecobani@gmail.com>
2024-01-17 16:26:15 +0200

    json-ify: 1/5 of 2/2

--- plugins/loadavg.py
+++ plugins/loadavg.py
@@ -10,28 +10,17 @@ from datetime import datetime
 from util.util import en_open
 from util.logger import setup_logger
 
-logger = setup_logger(__name__)
 
-logger.debug("[init] initializing")
-
-logger.debug("[open] /proc/loadavg")
-file = en_open("/proc/loadavg")
-
-logger.debug("[open] /proc/uptime")
-uptime_file = en_open("/proc/uptime")
-
-
-def uptime_format():
+# TODO: put function in the class maybe?
+def get_uptime(file):
     """format the uptime from seconds to a human readable format"""
 
     intervals = (("week", 604800), ("day", 86400), ("hour", 3600), ("minute", 60))
     result = []
 
-    uptime_file.seek(0)
-    logger.debug("[seek] /proc/uptime")
+    file.seek(0)
 
-    seconds = int(float(uptime_file.readline().split()[0]))
-
+    seconds = int(float(file.readline().split()[0]))
     original_seconds = seconds
 
     if seconds < 60:
@@ -50,6 +39,82 @@ def uptime_format():
     return (", ".join(result), original_seconds)
 
 
+class Loadavg:
+    def __init__(self):
+        """
+        initializing important stuff
+        """
+
+        self.logger = setup_logger(__name__)
+
+        self.logger.debug("[init] initializing")
+
+        self.loadavg_file = en_open("/proc/loadavg")
+        self.logger.debug("[open] /proc/loadavg")
+
+        self.uptime_file = en_open("/proc/uptime")
+        self.logger.debug("[open] /proc/uptime")
+
+        self.files_opened = [self.loadavg_file, self.uptime_file]
+
+    def close_files(self):
+        """
+        closing the opened files. always call this
+        when ending the program
+        """
+
+        for file in self.files_opened:
+            self.logger.debug(f"[close] {file.name}")
+            file.close()
+
+    def get_data(self):
+        """
+        returns a json dict with data
+        """
+
+        for file in self.files_opened:
+            file.seek(0)
+
+        loadavg_data = self.loadavg_file.read().split()
+        uptime_data = get_uptime(self.uptime_file)
+
+        data = {
+            "load_times": {
+                "1": loadavg_data[0],
+                "5": loadavg_data[1],
+                "15": loadavg_data[2],
+            },
+            "entities": {
+                "active": loadavg_data[3].split("/")[0],
+                "total": loadavg_data[3].split("/")[1],
+            },
+            "uptime": {
+                "since": datetime.fromtimestamp(time.time() - uptime_data[1]).strftime("%A %B %d %Y, %I:%M:%S %p"),
+                "uptime": uptime_data[0],
+            },
+        }
+
+        self.logger.debug("[get_data] return data")
+
+        return data
+
+    def print_data(self):
+        """
+        returns the data, but formatted.
+        not intended to be used, please
+        use get_data() instead
+        """
+
+        self.logger.debug("[data] print out")
+        data = self.get_data()
+
+        return (
+            f"  ——— /proc/loadavg {'—' * 47}\n"
+            f"     Load: {data['load_times']['1']}, {data['load_times']['5']}, {data['load_times']['15']}"
+            f"{' ':<6}| Procs: {data['entities']['active']} active, {data['entities']['total']} total"
+            f"\n   Uptime: {data['uptime']['uptime']}\n   Booted: {data['uptime']['since']}\n"
+        )
+
 def main():
     """/proc/loadavg - system load times and uptime"""
 
@@ -59,9 +124,7 @@ def main():
 
     uptime_func_out = uptime_format()
 
-    up_since_fmt = datetime.fromtimestamp(time.time() - uptime_func_out[1]).strftime(
-        "%A %B %d %Y, %I:%M:%S %p"
-    )
+    up_since_fmt = datetime.fromtimestamp(time.time() - uptime_func_out[1]).strftime("%A %B %d %Y, %I:%M:%S %p")
 
     file.seek(0)
     logger.debug("[seek] /proc/loadavv")
