52817a67242ca97af8e54335a787092557d56f53
devlocalhost <lazarecobani@gmail.com>
2024-02-05 00:21:24 +0200

    Revert "Merge pull request #6 from aleeeee1/dev"
    This reverts commit 6ef22a7178958601f2184c10b7c682d656a3a695, reversing
    changes made to 96c57df3d5119d1d9bc1549981ecc7691303f310.
    
    1. I find this very confusing. My knowledge isnt that advanced.
       Maybe this isnt a good idea. But, whats better? Understaning
       your code, even if its not the best, or, using someones else
       code, because its better than the current one, even though
       you dont understand it?
    
    2. This feels too much for me. Ive been trying to understand it
       but i simply cant. I wont push myself more.
    
    Alees method is indeed better, cause what if i change the
    structure of my code? That means, everyone else whos using it
    probably has to go through pain. So, ill just try my best to
    not make "breaking changes"

--- plugins/meminfo.py
+++ plugins/meminfo.py
@@ -8,7 +8,6 @@ from util.util import (
     en_open,
 )
 from util.logger import setup_logger
-from util.models import MeminfoData
 
 
 class Meminfo:
@@ -50,7 +49,7 @@ class Meminfo:
             self.logger.debug(f"[close] {file.name}")
             file.close()
 
-    def get_data(self) -> MeminfoData:
+    def get_data(self):
         """
         returns a json dict with data
         """
@@ -65,7 +64,77 @@ class Meminfo:
             for i in self.meminfo_file.readlines()
         )
 
-        data = MeminfoData(meminfo_data)
+        phy_memory_total = meminfo_data.get("MemTotal", 0)
+        phy_memory_available = meminfo_data.get("MemAvailable", 0)
+        phy_memory_free = meminfo_data.get("MemFree", 0)
+
+        phy_memory_raw_cached = meminfo_data.get("Cached", 0)
+        phy_memory_sreclaimable = meminfo_data.get("SReclaimable", 0)
+        phy_memory_buffers = meminfo_data.get("Buffers", 0)
+
+        phy_memory_cached = (
+            phy_memory_raw_cached + phy_memory_buffers + phy_memory_sreclaimable
+        )
+        phy_memory_actual_used = round(
+            phy_memory_total
+            - phy_memory_free
+            - phy_memory_buffers
+            - phy_memory_raw_cached
+            - phy_memory_sreclaimable
+        )
+        phy_memory_used = round(phy_memory_total - phy_memory_available)
+        phy_memory_used_percent = round(
+            (int(phy_memory_used) / int(phy_memory_total)) * 100, 1
+        )
+
+        swap_memory_total = meminfo_data.get("SwapTotal", 0)
+        swap_memory_available = meminfo_data.get("SwapFree", 0)
+        # NOTE: Swap available (in output) = this.
+        # NOTE: Rename to Free instead?
+
+        swap_memory_cached = meminfo_data.get("SwapCached", 0)
+        swap_memory_used = round(swap_memory_total - swap_memory_available)
+
+        swap_memory_used_percent = round(
+            (int(swap_memory_used) / int(swap_memory_total)) * 100, 1
+        )
+
+        data = {
+            "physical": {
+                "values": {
+                    "total": phy_memory_total,
+                    "used": phy_memory_used,
+                    "actual_used": phy_memory_actual_used,
+                    "available": phy_memory_available,
+                    "free": phy_memory_free,
+                    "cached": phy_memory_cached,
+                },
+                "percentage": {
+                    "used": phy_memory_used_percent,
+                    "actual_used": round(
+                        (int(phy_memory_actual_used) / int(phy_memory_total)) * 100, 1
+                    ),
+                    "available": round(100 - phy_memory_used_percent, 1),
+                    "free": round(
+                        (int(phy_memory_free) / int(phy_memory_total)) * 100, 1
+                    ),
+                },
+            },
+            "virtual": {
+                "values": {
+                    "total": swap_memory_total,
+                    "used": swap_memory_used,
+                    "available": swap_memory_available,
+                    "cached": swap_memory_cached,
+                },
+                "percentage": {
+                    "used": round(
+                        (int(swap_memory_used) / int(swap_memory_total)) * 100, 1
+                    ),
+                    "available": round(100 - swap_memory_used_percent, 1),
+                },
+            },
+        }
 
         return data
 
@@ -78,37 +147,37 @@ class Meminfo:
 
         data = self.get_data()
 
-        phys_memory_used_format = f"{convert_bytes(data.physical.values.used)} ({data.physical.percentage.used}%)"
-        phys_memory_avail_format = f"{convert_bytes(data.physical.values.available)} ({data.physical.percentage.available}%)"
+        phys_memory_used_format = f"{convert_bytes(data['physical']['values']['used'])} ({data['physical']['percentage']['used']}%)"
+        phys_memory_avail_format = f"{convert_bytes(data['physical']['values']['available'])} ({data['physical']['percentage']['available']}%)"
 
-        if data.virtual.values.total != 0 and SHOW_SWAP is not False:
+        if data["virtual"]["values"]["total"] != 0 and SHOW_SWAP is not False:
             combined_total_memory = (
-                data.physical.values.total + data.virtual.values.total
+                data["physical"]["values"]["total"] + data["virtual"]["values"]["total"]
             )
             combined_total_actual_used = (
-                data.physical.values.actual_used
-                + data.virtual.values.used
+                data["physical"]["values"]["actual_used"]
+                + data["virtual"]["values"]["used"]
             )
             combined_total_used = (
-                data.physical.values.used + data.virtual.values.used
+                data["physical"]["values"]["used"] + data["virtual"]["values"]["used"]
             )
             combined_total_available = (
-                data.physical.values.available
-                + data.virtual.values.available
+                data["physical"]["values"]["available"]
+                + data["virtual"]["values"]["available"]
             )
 
             combined_used_percent = round(
                 (
-                    data.physical.percentage.used
-                    + data.virtual.percentage.used
+                    data["physical"]["percentage"]["used"]
+                    + data["virtual"]["percentage"]["used"]
                 )
                 / 2,
                 1,
             )
             combined_available_percent = round(
                 (
-                    data.physical.percentage.available
-                    + data.virtual.percentage.available
+                    data["physical"]["percentage"]["available"]
+                    + data["virtual"]["percentage"]["available"]
                 )
                 / 2,
                 1,
@@ -117,19 +186,19 @@ class Meminfo:
             return (
                 f"  ——— /proc/meminfo {'—' * 47}\n"
                 f"     RAM: {' ' * 25}Swap:\n"
-                f"         Total: {convert_bytes(data.physical.values.total)}"
-                + f"{' ':<16}Total: {convert_bytes(data.virtual.values.total)}\n"
+                f"         Total: {convert_bytes(data['physical']['values']['total'])}"
+                + f"{' ':<16}Total: {convert_bytes(data['virtual']['values']['total'])}\n"
                 f"          Used: {phys_memory_used_format}"
                 + " " * (25 - len(phys_memory_used_format))
-                + f"Used: {convert_bytes(data.virtual.values.used)} ({data.virtual.percentage.used}%)\n"
-                f"   Actual Used: {convert_bytes(data.physical.values.actual_used)} ({data.physical.percentage.actual_used}%)\n"
+                + f"Used: {convert_bytes(data['virtual']['values']['used'])} ({data['virtual']['percentage']['used']}%)\n"
+                f"   Actual Used: {convert_bytes(data['physical']['values']['actual_used'])} ({data['physical']['percentage']['actual_used']}%)\n"
                 f"     Available: {phys_memory_avail_format}"
                 + " " * (20 - len(phys_memory_avail_format))
-                + f"Available: {convert_bytes(data.virtual.values.available)} ({data.virtual.percentage.available}%)\n"
-                f"          Free: {convert_bytes(data.physical.values.free)} ({data.physical.percentage.free}%)\n"
-                f"        Cached: {convert_bytes(data.physical.values.cached)}"
-                + " " * (23 - len(convert_bytes(data.physical.values.cached)))
-                + f"Cached: {convert_bytes(data.virtual.values.cached)}\n   — Combined: {'— ' * 26}\n"
+                + f"Available: {convert_bytes(data['virtual']['values']['available'])} ({data['virtual']['percentage']['available']}%)\n"
+                f"          Free: {convert_bytes(data['physical']['values']['free'])} ({data['physical']['percentage']['free']}%)\n"
+                f"        Cached: {convert_bytes(data['physical']['values']['cached'])}"
+                + " " * (23 - len(convert_bytes(data["physical"]["values"]["cached"])))
+                + f"Cached: {convert_bytes(data['virtual']['values']['cached'])}\n   — Combined: {'— ' * 26}\n"
                 + f"         Total: {convert_bytes(combined_total_memory)}{' ':<17}Used: {convert_bytes(combined_total_used)} ({combined_used_percent}%)\n"
                 f"     Available: {convert_bytes(combined_total_available)} ({combined_available_percent}%){' ':<2}Actual Used: {convert_bytes(combined_total_actual_used)}\n"
             )
@@ -137,11 +206,11 @@ class Meminfo:
         return (
             f"  ——— /proc/meminfo {'—' * 47}\n"
             f"   RAM: {' ' * 25}\n"
-            f"        Total: {convert_bytes(data.physical.values.total)}"
-            + f"{' ':<17}Cached: {convert_bytes(data.physical.values.cached)}\n"
-            f"         Used: {convert_bytes(data.physical.values.used)} ({data.physical.percentage.used}%)"
+            f"        Total: {convert_bytes(data['physical']['values']['total'])}"
+            + f"{' ':<17}Cached: {convert_bytes(data['physical']['values']['cached'])}\n"
+            f"         Used: {convert_bytes(data['physical']['values']['used'])} ({data['physical']['percentage']['used']}%)"
             + " " * (20 - len(str(phys_memory_used_format)))
-            + f"Actual Used: {convert_bytes(data.physical.values.actual_used)} ({data.physical.percentage.actual_used}%)\n"
-            f"    Available: {convert_bytes(data.physical.values.available)} ({data.physical.percentage.available}%)"
-            + f"{' ':<9}Free: {convert_bytes(data.physical.values.free)} ({data.physical.percentage.free}%)\n"
+            + f"Actual Used: {convert_bytes(data['physical']['values']['actual_used'])} ({data['physical']['percentage']['actual_used']}%)\n"
+            f"    Available: {convert_bytes(data['physical']['values']['available'])} ({data['physical']['percentage']['available']}%)"
+            + f"{' ':<9}Free: {convert_bytes(data['physical']['values']['free'])} ({data['physical']['percentage']['free']}%)\n"
         )
