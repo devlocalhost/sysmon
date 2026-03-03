7f6a75b26fec2fb3e598cde6be77756805ae06d6
devlocalhost <lazarecobani@gmail.com>
2026-03-03 13:45:22 +0200

    update logger spacing

--- utils/logger.py
+++ utils/logger.py
@@ -10,7 +10,7 @@ def define_logger(plugin_name):
         # level=logging.DEBUG if util.DEBUGGING else logging.INFO,
         level=logging.DEBUG,
         datefmt="%X",
-        format="%(asctime)s.%(msecs)03d - %(name)-16s %(funcName)-16s -> %(message)s",
+        format="%(asctime)s.%(msecs)03d - %(name)-16s %(funcName)-30s -> %(message)s",
         filemode="w",
     )
 
