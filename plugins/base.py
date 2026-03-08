d0bab8808011cd47d482b66a742ce5361322fd97
devlocalhost <lazarecobani@gmail.com>
2026-03-08 21:10:52 +0200

    remove obsolete comments

--- plugins/base.py
+++ plugins/base.py
@@ -9,16 +9,16 @@ class BasePlugin:
 
         self.logger = create_logger(self.__class__.__name__)
         self._opened_files = []
-        # maybe this should be a function instead?
-        # try opening file. if it opens, log
-        # else log, plus exception. looks/feels cleaner to me
 
     def _seek_files(self):
         for file in self._opened_files:
             self.logger.debug(f"seeking {file.name}")
-            file.seek(0)
-            # maybe i should use a try except block?
-            # im already handling that in the plugins though...
+            
+            try:
+                file.seek(0)
+
+            except Exception as exc:
+                self.logger.debug(f"error seeking file: {exc}")
 
     def _open_file(self, file, mode="r", encoding="utf-8"):
         """modifying the default open method so i dont have to define encoding every time"""
