35c79c88fc5c83a84acf5c394ede7803efea9011
devlocalhost <lazarecobani@gmail.com>
2026-03-09 20:21:20 +0200

    initial config
    but to be honest, i dont really like the way i implemented it

--- utils/logger.py
+++ utils/logger.py
@@ -1,14 +1,21 @@
 import logging
 
-# from . import util
+# this part feels ugly to me. i do it so nicely in pyspodl...
+from .config import Config, ConfigError
 
+config = Config().read_config()
+
+try:
+    DEBUGGING = config["misc"]["debugging"]
+
+except ConfigError:
+    DEBUGGING = False
+# this part feels ugly to me. i do it so nicely in pyspodl...
 
 def create_logger(plugin_name):
     logging.basicConfig(
-        filename="sysmon.log",
-        # filename="sysmon.log" if util.DEBUGGING else "/dev/null",
-        # level=logging.DEBUG if util.DEBUGGING else logging.INFO,
-        level=logging.DEBUG,
+        filename="sysmon.log" if DEBUGGING else "/dev/null",
+        level=logging.DEBUG if DEBUGGING else logging.INFO,
         datefmt="%X",
         format="%(asctime)s.%(msecs)03d - %(name)-16s %(funcName)-30s -> %(message)s",
         filemode="w",
